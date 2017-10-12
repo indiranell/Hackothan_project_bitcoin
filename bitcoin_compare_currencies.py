ad09bca99c41e4c310a9e3260f19f4c0b2451429
Indira Nellutla <indira@qxf2.com>
2017-10-12 18:42:00 +0530

    Add files via upload

---
+++ bitcoin_compare_currencies.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import quandl
+
+#Quandl api key
+quandl.ApiConfig.api_key = 'HbS8azUsnsRnb_d17E1Z'
+
+#Read different markets for USD
+df_usd_bitstamp = quandl.get("BCHARTS/BITSTAMPUSD", start_date="2016-09-12", end_date="2017-10-12", returns="pandas")
+df_usd_kraken = quandl.get('BCHARTS/KRAKENUSD',start_date="2016-09-12", end_date="2017-10-12", returns="pandas")
+
+#Plot using matplotlib library
+f, (ax1, ax2) = plt.subplots(1, 2)
+plt.suptitle("Bitcoin Open and close Price of different markets in USD")
+ax1.set_title('BITSTAMP Market')
+ax2.set_title('KRAKEN Market')
+#Plot BITSTAMP
+plt.legend(['Open','Close'], loc='upper right')
+ax1.plot(df_usd_bitstamp[['Open']], color='r')
+ax1.plot(df_usd_bitstamp[['Close']], color='g')
+ax1.set_xlabel('Year', size =14,va = 'center')
+ax1.set_ylabel('Price(in USD)', size =14,va = 'center')
+#Plot KRAKEN
+ax2.plot(df_usd_kraken[['Open']], color='r')
+ax2.plot(df_usd_kraken[['Close']], color='g')
+ax2.set_xlabel('Year', size =14,va = 'center')
+ax2.set_ylabel('Price(in USD)', size =14,va = 'center')
+plt.legend()
+
+#data[['High','Low','Open','Close']].plot()
+#plt.show()
+plt.show()
---
+++ bitcoin_compare_markets.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import quandl
+# Using Matplotlib to plot different markets in US
+
+#Quandl api key
+quandl.ApiConfig.api_key = 'HbS8azUsnsRnb_d17E1Z'
+
+# Pull pricing data for 3 more BTC exchanges
+exchanges = ['COINBASE','BITSTAMP','ITBIT','KRAKEN']
+
+exchange_data = {}
+
+for exchange in exchanges:
+    exchange_code = 'BCHARTS/{}USD'.format(exchange)
+    btc_exchange_df = quandl.get(exchange_code)
+    exchange_data[exchange] = btc_exchange_df
+
+def merge_dfs_on_column(dataframes, labels, col):
+    #Merge a single column of each dataframe into a new combined dataframe
+    series_dict = {}
+    for index in range(len(dataframes)):
+        series_dict[labels[index]] = dataframes[index][col]
+        
+    return pd.DataFrame(series_dict)
+
+df_usd_datasets = merge_dfs_on_column(list(exchange_data.values()), list(exchange_data.keys()), 'Weighted Price')
+
+print (df_usd_datasets.tail())
+
+df_usd_datasets.plot()
+plt.title("Bitcoin Price (USD) comparision for different markets")
+plt.show()
